de2ae94ea062f578e4bca3e7860b4d14118facb5
hmsou <hmarcs66@gmail.com>
2025-10-31 21:00:53 -0300

    feat: add sequences and next-characters for prediction

--- main.py
+++ main.py
@@ -13,3 +13,13 @@ text = text[300000:800000]
 characters = sorted(set(text))
 char_to_index = dict((c, i) for i, c in enumerate(characters))
 index_to_char = dict((i, c) for i, c in enumerate(characters))
+
+SEQ_LENGTH = 40
+STEP_SIZE = 3
+
+sentences = []
+next_characters = []
+
+for i in range(0, len(text) - SEQ_LENGTH, STEP_SIZE):
+    sentences.append(text[i: i+SEQ_LENGTH])
+    next_characters.append(text[i+SEQ_LENGTH])
